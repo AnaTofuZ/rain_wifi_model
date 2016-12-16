7cc869ef66a3ca7dba80b597e033b1cdea6b937a
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2016-12-16 21:27:31 +0900

    modified sor

--- sor.pl
+++ sor.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use utf8;
 use Data::Dumper;
-
+use 5.23.9;
 
 my $Ghz18 = "192.168.100.9";
 my $Ghz26 = "192.168.100.11";
@@ -19,56 +19,57 @@ print "RxData/200906/20090601/"."$Ghz18"."_csv.log";
 open(IN,"RxData/200906/20090601/"."$Ghz18"."_csv.log");
 
 my $count = 0;
+my $goal=0;
+my $sum=0;
+
+my ($hour,$min,$second,$level) = (0,0,0,0);
+my ($f_hour,$f_min,$f_second,$f_level) = (0,0,0,0);
 
 while(<IN>){
     chomp;
     if($_ eq "HOST NAME : Undefined Host  IP ADDRESS : 192.168.100.9"){ next;}
     if($_ eq "time, 1803_RX_LEVEL, 1803_TX_POWER, 1803_BER"){ next;}
     
-    my @tmp = split /\:|\,/,$_;
+    ($hour,$min,$second,$level) = split /\:|\,/,$_;
+
+    if(! ($hour =~ /^\d+$/)){
+        next;
+        
+    }
+
+    if($count == 0){ 
+
+    $f_hour = $hour;
+    $f_min = $min;
+    $f_second = $second;
+    $goal = $f_second +8;
+    $goal= $goal >= 60 ? $goal % 60 : $goal;
+    $sum=$level;
+
+    $count++; next; }
+
+    $sum+=$level;
+
+    if($goal <= $second){
+#        print "$goal\n";
+#        print "$second\n";
+     $sum /=$count;
+     $result->{"$f_hour".":"."$f_min".":"."$f_second"}="$sum";
+     $count=0;
+     next;
+    }
+=commnet
     push(@array_hour,shift @tmp);
     push(@array_min,shift @tmp);
     push(@array_second,shift @tmp);
     push(@array_level,shift @tmp);
-
-#    ( $array_hour->["$count"],$array_min->["$count"],$array_second->["$count"],$array_level->["$count"]) = split /\:|\,/,$_;
+=cut
 
    $count ++;
 
 }
 
-
-#print $start_min;
-
-my $total_second =0;
-my $goal =0;
-
-for (my $i = 0; $i < scalar(@$array_hour); $i++) {
-    
-
-    $total_second += $array_second->[$i];
-
-    if($total_second >= 60){
-        $total_second %=60;
-    }
-
-    if($total_second < $goal){
-        next;
-    }else{
-    
-        print $start_second;
-#        print "$text\n";
-#     $result->{$start_hour".":"."$start_min".":"."$start_second}="$total_second";
-       
-    ($start_hour,$start_min,$start_second) = $array_hour->[$i+1],$array_min->[$i+1],$array_second->[$i+1];
-
-    $goal = $start_second+ 10;
-    if($goal >= 60 ){
-        $goal %= 60;
-    }
-    }
-
-
-}
-#print @$array_level;
-
+#print Dumper($result);
+   for my $key(sort keys %$result){
+        printf("%s %s\n",$key,$result->{$key});
+    } 
