62012865b57379a2edb1bc87bb7bb173ba5e0007
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2016-12-20 21:52:07 +0900

    modified *.pl

--- rain.pl
+++ rain.pl
@@ -4,73 +4,64 @@ use warnings;
 use utf8;
 use Data::Dumper;
 
-for(20090601..20090630){
-    my $txt ="$_"."/"."$_"."_rain.csv";
-    rian_data($txt,$_);
-}
 
-for(20091002..20091031){
-    my $txt ="$_"."/"."$_"."_rain.csv";
-    rian_data($txt,$_);
-}
+my @total =(20090601..20090630,20091002..20091031,20091101..20091130,20091201..20091230);
 
-for(20091101..20091130){
-    my $txt ="$_"."/"."$_"."_rain.csv";
-    rian_data($txt,$_);
-}
 
-for(20091201..20091230){
+for (@total){
+
     my $txt ="$_"."/"."$_"."_rain.csv";
     rian_data($txt,$_);
+
 }
 
 
 sub rian_data{
-my($txtname,$num) = @_;
+    my($txtname,$num) = @_;
 
-if(! -e $txtname){
-    return;
-}
-
-open(IN,$txtname);
-
-my $count = 1;
-my $result ={};
-my $rain_sum = 0;
-my $time_b;
- 
-while(<IN>){
-    chomp;
-    if($_ eq ""){ next;}
-    if($_ eq "Recording started."){ next;}
-
-    my($day,$time_r) = split / /,$_;
-    my($time,$rain) = split /,/,$time_r;
-
-    if($count == 1){
-        $time_b = $time;}
-
-    $rain_sum += $rain;
-
-    if($count == 60){
-        $rain_sum/=60;
-        $result->{"$time_b"}=sprintf('%.2f',$rain_sum);
-        $count =0;
-        $rain_sum =0;
+    if(! -e $txtname){
+        return;
     }
 
-    $count++;
-    
-}
+    open(IN,$txtname);
 
-close(IN);
+    my $count = 1;
+    my $result ={};
+    my $rain_sum = 0;
+    my $time_b;
+     
+    while(<IN>){
+        chomp;
+        next if $_ eq "";
+        next if $_ eq "Recording started.";
 
-my $textfile = "$num".".txt";
-open(OUT,"> $textfile");
+        my($day,$time_r) = split / /,$_;
+        my($time,$rain) = split /,/,$time_r;
 
-    for my $key(sort keys %$result){
-        printf(OUT "%s %s\n",$key,$result->{$key});
+        if($count == 1){
+            $time_b = $time;}
+
+        $rain_sum += $rain;
+
+        if($count == 60){
+            $rain_sum*=0.0083333;
+            $result->{"$time_b"}=sprintf('%.2f',$rain_sum);
+            $count =0;
+            $rain_sum =0;
+        }
+
+        $count++;
+        
     }
+
+    close(IN);
+
+    my $textfile = "$num.txt";
+    open(OUT,"> $textfile");
+
+        for my $key(sort keys %$result){
+            printf(OUT "%s %s\n",$key,$result->{$key});
+        }
 #print OUT Dumper($result);
-close(OUT);
+    close(OUT);
 }
--- sort.pl
+++ sort.pl
@@ -7,73 +7,93 @@ use 5.23.9;
 
 my $Ghz18 = "192.168.100.9";
 my $Ghz26 = "192.168.100.11";
-my @array_hour =();
-my @array_min = ();
-my @array_second =();
-my @array_level = ();
 
-my $result = {};
+my @directory = (200906,200910,200911,200912);
 
-#print "RxData/200906/20090601/"."$Ghz18"."_csv.log";
- 
-open(IN,"RxData/200906/20090601/"."$Ghz18"."_csv.log");
-
-my $count = 0;
-my $goal=0;
-my $sum=0;
-
-my ($hour,$min,$second,$level) = (0,0,0,0);
-my ($f_hour,$f_min,$f_second,$f_level) = (0,0,0,0);
-
-while(<IN>){
-    chomp;
-#    if($_ eq "HOST NAME : Undefined Host  IP ADDRESS : 192.168.100.9"){ next;}
-#    if($_ eq "time, 1803_RX_LEVEL, 1803_TX_POWER, 1803_BER"){ next;}
-    if($. eq 1..2){next;}
-    
-    ($hour,$min,$second,$level) = split /\:|\,/,$_;
-
-    if(! ($hour =~ /^\d+$/)){
-        next;
-        
+for  my $roop (@directory){
+    for (01..31){
+        $_ = sprintf("%02d",$_);
+        my $name = $roop."/".$roop.$_;
+        &count($Ghz18,$name);
+        &count($Ghz26,$name);
     }
-    if(! ($level =~ /^(-)\d+$/)){
-        next;
-    } 
-
-    if($count == 0){ 
-
-    $f_hour = $hour;
-    $f_min = $min;
-    $f_second = $second;
-    $goal = $f_second +8;
-    $goal= $goal >= 60 ? $goal % 60 : $goal;
-    $sum=$level;
-
-    $count++; next; }
-
-    $sum+=$level;
-
-    if($goal <= $second){
-#        print "$goal\n";
-#        print "$second\n";
-     $sum /=($count+1);
-     $result->{"$f_hour".":"."$f_min".":"."$f_second"}="$sum";
-     $count=0;
-     next;
-    }
-=commnet
-    push(@array_hour,shift @tmp);
-    push(@array_min,shift @tmp);
-    push(@array_second,shift @tmp);
-    push(@array_level,shift @tmp);
-=cut
-
-   $count ++;
-
 }
 
-#print Dumper($result);
-   for my $key(sort keys %$result){
-        printf("%s %s\n",$key,$result->{$key});
-    } 
+sub count{ 
+
+
+    my($Ghz,$path) = @_;
+
+    my @array_hour =();
+    my @array_min = ();
+    my @array_second =();
+    my @array_level = ();
+
+    my $result = {};
+   
+
+    if(! -e "RxData/".$path."/"."$Ghz"."_csv.log"){
+        return;
+    }
+
+    open(IN,"RxData/".$path."/"."$Ghz"."_csv.log");
+
+    my $goal=0;
+    my $sum=0;
+    my $count=0;
+
+    my ($hour,$min,$second,$level) = (0,0,0,0);
+    my ($f_hour,$f_min,$f_second,$f_level) = (0,0,0,0);
+
+    while(<IN>){
+        chomp;
+    #    if($_ eq "HOST NAME : Undefined Host  IP ADDRESS : 192.168.100.9"){ next;}
+    #    if($_ eq "time, 1803_RX_LEVEL, 1803_TX_POWER, 1803_BER"){ next;}
+        next if($. eq 1..2);
+        
+        ($hour,$min,$second,$level) = split /\:|\,/,$_;
+
+        next if(! ($hour =~ /^[0-9]+$/));
+        
+        next if(! defined $level);
+
+        next if(!($level =~ /^-?[0-9]+\.?[0-9]+$/));
+
+        if($count == 0){ 
+
+        $f_hour = $hour;
+        $f_min = $min;
+        $f_second = $second;
+        $goal = $f_second +8;
+        $goal= $goal >= 60 ? $goal % 60 : $goal;
+        $sum=$level;
+
+        $count++; next; }
+
+        $sum+=$level;
+
+        if($goal <= $second){
+    #        print "$goal\n";
+    #        print "$second\n";
+         $sum /=($count+1);
+         $result->{"$f_hour".":"."$f_min".":"."$f_second"}="$sum";
+         $count=0;
+         next;
+        }
+
+       $count ++;
+
+    }
+
+    close(IN);
+    my $tmp = "RxData/".$path."/"."$Ghz"."_csv_sort.log";
+    open(OUT,"> $tmp");
+
+    #print Dumper($result);
+       for my $key(sort keys %$result){
+            printf(OUT "%s %s\n",$key,$result->{$key});
+        } 
+
+        close(OUT);
+
+}
