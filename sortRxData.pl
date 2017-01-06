d0bfd2e9155c8a65ab37030fba0173e76d589a6f
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-06 15:29:39 +0900

    make sortRxData.pl

---
+++ sortRxData.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use utf8;
+use constant;
+
+use Data::Dumper;
+use 5.23.9;
+
+use constant Ghz18 => "192.168.100.9";
+use constant Ghz26 => "192.168.100.11";
+
+use constant @directory =>(200906,200910,200911,200912);
+
+
+
