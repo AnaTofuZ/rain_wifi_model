c52c02e275509215997b70e9e822554c016f7b88
shimitakax <e155730@ie.u-ryukyu.ac.jp>
2017-01-12 13:06:32 +0900

    modified used constant

--- sortRxData.pl
+++ sortRxData.pl
@@ -15,8 +15,10 @@ use constant directory =>(200906,200910,200911,200912);
 my $sumGhz18 = {};
 my $sumGhz26 = {};
 
+my $stride = 5;
+
 for  (-100..0){
-    next if $_ %5 != 0;
+    next if $_ % $stride!= 0;
     $sumGhz18->{$_}=0;
     $sumGhz26->{$_}=0;
 }
@@ -25,22 +27,24 @@ for my $roop (directory){
     for  (01..31){
         $_ = sprintf("%02d",$_);
         my $name = "$roop/$roop$_";
-        &sumGhz18($name,$sumGhz18);    
+        &sumGhz18($name,$sumGhz18);  
+        &sumGhz26($name,$sumGhz26);  
     }
 }
 
 print Dumper $sumGhz18;
+print Dumper $sumGhz26;
 
 sub sumGhz18 {
 
     my($path,$result) = @_;
 
-    open(IN,"RxData/$path/192.168.100.9_csv_sort.log");
+    open(IN,"RxData/$path/".Ghz18."_csv_sort.log");
 
    while (<IN>) {
        chomp;
        $_ =(split / /,$_)[1];  
-       $_=5*int($_/5);
+       $_= $stride*int($_/$stride);
        $result->{$_}++;
    } 
    close IN;
@@ -49,12 +53,12 @@ sub sumGhz18 {
 sub sumGhz26 {
     my($path,$result) = @_;
 
-    open(IN,"RxData/$path/$Ghz26_csv_sort.log");
+    open(IN,"RxData/$path/".Ghz26."_csv_sort.log");
 
    while (<IN>) {
        chomp;
         $_ =(split / /,$_)[1];
-        $_=5*int($_/5);
+        $_=$stride*int($_/$stride);
         $result->{$_}++;
     }
     close IN;
